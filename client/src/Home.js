3c3d4f0d4604bbf26fe796dc8c7c1736694fd8a9
aaweeza <sp22bscs0077@maju.edu.pk>
2024-05-29 13:05:39 +0500

    my_rest_api_project

---
+++ client/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+        <div className="overlay">
+        <h1>Welcome to Crafty Quill</h1>
+        <p>Explore our wide range of premium stationary!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
